6e0999a6ef1285efe37f956dd0fc7490828d69de
NavindaHewawickrama <hewawickraman@gmail.com>
2026-02-14 08:55:14 +0530

    changes 3

--- src/App.jsx
+++ src/App.jsx
@@ -358,7 +358,7 @@ function App() {
                       initial={{ opacity: 0, x: 100 }}
                       animate={{ opacity: 1, x: 0 }}
                       exit={{ opacity: 0, x: -100 }}
-                      transition={{ duration: 0.3 }}
+                      transition={{ duration: 0.15 }}
                       className="md:w-[25%] lg:w-[25%] sm:w-full max-w-md"
                     >
                       <video
